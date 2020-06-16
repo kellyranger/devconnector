ef9873547b7b7196d03cd5160570048e81c3db55
Kelly Ranger <kelly@losgatos-massage.com>
2020-06-16 15:42:29 -0700

    repos array not filling

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import {getProfileById} from '../../actions/profile';
 
 const Profile = ({
@@ -61,6 +62,9 @@ const Profile = ({
               <h4>No Education</h4>
             )}
           </div>
+          {profile.githubusername && (
+            <ProfileGithub  username={profile.githubusername}/>
+          )}
         </div>
         </Fragment>}
     </Fragment>
