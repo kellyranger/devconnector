c89345ce7fdf3e7633977b9d3c1adca512d96360
Kelly Ranger <kelly@losgatos-massage.com>
2020-06-20 21:38:17 -0700

    second click on a like creates TypeError: posts.map is not a function

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,7 +3,8 @@ import {setAlert} from './alert';
 import {
   GET_POSTS,
   POST_ERROR,
-  PROFILE_ERROR
+  PROFILE_ERROR,
+  UPDATE_LIKES
 } from './types';
 
 //Get posts
@@ -14,6 +15,40 @@ export const getPosts = () => async dispatch => {
    dispatch({
      type: GET_POSTS,
      payload: res.data
+   });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
+
+//Add Like
+export const addLike = id => async dispatch => {
+  try {
+   const res = await axios.put(`/api/posts/like/${id}`);
+
+   dispatch({
+     type: UPDATE_LIKES,
+     payload: { id, likes: res.data }
+   });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
+
+//Remove Like
+export const removeLike = id => async dispatch => {
+  try {
+   const res = await axios.put(`/api/posts/unlike/${id}`);
+
+   dispatch({
+     type: UPDATE_LIKES,
+     payload: { id, likes: res.data }
    });
   } catch (err) {
     dispatch({
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,7 +1,7 @@
 import {
   GET_POSTS,
-  POST_ERROR
-
+  POST_ERROR,
+  UPDATE_LIKES
 } from '../actions/types';
 
 
@@ -30,6 +30,14 @@ export default function(state = initialState, action){
         posts: payload,
         loading: false
       };
+      case UPDATE_LIKES:
+        return {
+          ...state,
+          posts: state.posts.map(post => 
+            post._id === payload.id ? {...post, likes:
+          payload.likes } : post),
+          loading: false 
+        };
       default:
         return {
           ...state
